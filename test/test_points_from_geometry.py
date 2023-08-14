f14476b184bbdcbd0971ff77d272b643d84083ff
Sadkov_SA <SergeiSadkov@terratech.ru>
2023-08-14 08:45:02 +0300

    Test get_coordinates functions

---
+++ test/test_points_from_geometry.py
@@ -0,0 +1,35 @@
+"""
+Testing problem180meridian.points_from_wkt()
+"""
+
+from osgeo import ogr
+
+from problem180meridian import points_from_geometry
+
+
+def check_parsing(geometry):
+    points = points_from_geometry(geometry)
+    if points is not None:
+        print(points)
+
+
+test_ds = ogr.Open('./test/test_data/test_geometry.geojson')
+test_lr = test_ds.GetLayer()
+
+
+# Testing POINT geometry for cross180 - None is expected
+
+def test_point():
+    check_parsing(test_lr.GetFeature(1).GetGeometryRef()) is None
+
+
+# Testing LINESTRING geometry for cross180 - None is expected
+
+def test_linestring():
+    check_parsing(test_lr.GetFeature(0).GetGeometryRef()) == [[[(24.5415438402914, 13.5588583164334), (25.5100146734532, 4.19366085458475),(32.6431265127727, 6.13284981165079)]]]
+
+
+# Testing POLYGON geometry for cross180 - None is expected
+
+def test_polygon():
+    check_parsing(test_lr.GetFeature(2).GetGeometryRef()) == [[[(42.2673084292096, 59.944926664255), (75.0211577247333, 62.6922826805411), (47.1942850696495, 67.2099881248319), (23.0620579829856, 63.2777691933206), (26.5176224655007, 66.1661342312123), (15.0717845363254, 58.7370412696996), (42.2673084292096, 59.9449266642551)]]]
---
+++ test/test_points_from_wkt.py
@@ -0,0 +1,45 @@
+"""
+Testing problem180meridian.points_from_wkt()
+"""
+
+from problem180meridian import points_from_wkt
+
+
+def check_parsing(wkt):
+    points = points_from_wkt(wkt)
+    if points is not None:
+        print(points)
+
+
+# Testing point wkt for cross180 - None is expected
+
+def test_point():
+    check_parsing('POINT (10 20)') is None
+
+def test_multipoint():
+    check_parsing('MULTIPOINT ((10 40), (40 30), (20 20), (30 10))') is None
+    check_parsing('MULTIPOINT (10 40, 40 30, 20 20, 30 10)') is None
+
+
+# Testing non processed wkt types for cross180 - None is expected
+
+def test_circularstring():
+    check_parsing('CIRCULARSTRING (1 5, 6 2, 7 3)') is None
+
+
+# Testing empty wkt for cross180 - None is expected
+
+def test_empty_polygon():
+    check_parsing('POLYGON EMPTY') is None
+
+
+# Testing linear wkt
+
+def test_linestring():
+    check_parsing('LINESTRING (10 20, 20 30)') == [[[(10.0, 20.0), (20.0, 30.0)]]]
+
+
+# Testing polygon wkt
+
+def test_polygon():
+    check_parsing('MULTIPOLYGON (((40 40, 20 45, 45 30, 40 40)), ((20 35, 10 30, 10 10, 30 5, 45 20, 20 35), (30 20, 20 15, 20 25, 30 20)))') == [[[(40.0, 40.0), (20.0, 45.0), (45.0, 30.0), (40.0, 40.0)]], [[(20.0, 35.0), (10.0, 30.0), (10.0, 10.0), (30.0, 5.0), (45.0, 20.0), (20.0, 35.0)], [(30.0, 20.0), (20.0, 15.0), (20.0, 25.0), (30.0, 20.0)]]]
